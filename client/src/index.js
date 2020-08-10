55bd2c917bcc03e7b90a21e7adb1131bd137afdb
abhishektanwar <abhishektanwar084@gmail.com>
2020-08-10 19:37:30 +0530

    make dropdown component

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+// import App from './App';
+import LoadingContainer from './LoadingContainer.js'
+
+
+ReactDOM.render(
+    <LoadingContainer />,
+  document.getElementById('root')
+);
